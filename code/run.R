d3fe8702473e9d2b63afd1cbf02d55b538831bb3
lucas-johnsonn <lucas.k.johnson03@gmail.com>
2021-10-27 16:54:13 -0400

    lgb; report start; run script

---
+++ code/run.R
@@ -0,0 +1,9 @@
+library(here)
+ksource <- function(x, ...) {
+  source(knitr::purl(x, output = tempfile()), ...)
+}
+
+ksource(here("code/logistic_regression.Rmd"))
+ksource(here("code/random_forest.Rmd"))
+ksource(here("code/lightgbm.Rmd"))
+ksource(here("report/report.Rmd"))
